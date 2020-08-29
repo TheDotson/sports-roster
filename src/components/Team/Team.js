cf9652a1684aa551a7bb4fa542118a93fa749178
Michael Dotson <thedotson@icloud.com>
2020-08-29 13:40:28 -0500

    edit player function works

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -13,6 +13,7 @@ class Team extends React.Component {
   state = {
     players: [],
     formOpen: false,
+    editPlayer: {},
   }
 
   getPlayers = () => {
@@ -43,19 +44,32 @@ class Team extends React.Component {
       .catch((err) => console.error('Create player failed', err));
   }
 
+  editAPlayer = (playerToEdit) => {
+    this.setState({ formOpen: true, editPlayer: playerToEdit });
+  }
+
+  updatePlayer = (playerId, editedPlayer) => {
+    playerData.editPlayer(playerId, editedPlayer)
+      .then(() => {
+        this.getPlayers();
+        this.setState({ formOpen: false, editPlayer: {} });
+      })
+      .catch((err) => console.error(err));
+  }
+
   closeForm = () => {
     this.setState({ formOpen: false });
   }
 
   render() {
-    const { players, formOpen } = this.state;
-    const playerCard = players.map((player) => <Player key={player.id} player={player} deletePlayer={this.deletePlayer}/>);
+    const { players, formOpen, editPlayer } = this.state;
+    const playerCard = players.map((player) => <Player key={player.id} player={player} deletePlayer={this.deletePlayer} editAPlayer={this.editAPlayer}/>);
 
     return (
       <div>
         <h1>Average Joe's Roster</h1>
-        { !formOpen ? <button className="btn btn-secondary mb-2" onClick={() => { this.setState({ formOpen: true }); }}>Create New Player</button> : '' }
-        { formOpen ? <PlayerForm createPlayer={this.createPlayer} closeForm={this.closeForm}/> : '' }
+        { !formOpen ? <button className="newPlayer btn btn-warning mb-2" onClick={() => { this.setState({ formOpen: true, editPlayer: {} }); }}>Create New Player</button> : '' }
+        { formOpen ? <PlayerForm createPlayer={this.createPlayer} editPlayer={editPlayer} updatePlayer={this.updatePlayer} closeForm={this.closeForm}/> : '' }
         <div className="rosterContainer">
           {playerCard}
         </div>
